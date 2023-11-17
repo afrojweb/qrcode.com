b9011b6ea0032b3dc5cde90ead70478b902c67b7
afrojweb <109802719+afrojweb@users.noreply.github.com>
2023-11-18 02:20:16 +0530

    Create index.js
    added index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const otpGenerator = require('otp-generator');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/generate-otp', (req, res) => {
+    const { mobileNumber } = req.body;
+    const otp = otpGenerator.generate(6, { upperCase: false, specialChars: false, alphabets: false });
+
+    // In a real application, you would send the OTP to the user's mobile number.
+    // Here, we're just rendering the OTP and passing the mobileNumber to the otp.html page.
+    res.render(__dirname + '/otp.html', { mobileNumber, otp });
+});
+
+app.post('/verify-otp', (req, res) => {
+    const { otp, mobileNumber } = req.body;
+
+    // In a real application, you would verify the entered OTP with the generated OTP.
+    // Here, we're just rendering a success page if the OTP matches.
+    res.send(`OTP Verified Successfully for Mobile Number: ${mobileNumber}`);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
